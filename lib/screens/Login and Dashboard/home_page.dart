43756beb5862e7f5029fd9c9894ec718fb5bd720
Piyush Agrawal <piyushagrawal691@gmail.com>
2021-03-03 16:36:19 +0530

    Created Login and Dashboard Pages

---
+++ lib/screens/Login and Dashboard/home_page.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'Components/appBar.dart';
+import 'Components/side_drawer.dart';
+import 'DashboardComponents/cardItems.dart';
+class HomePage extends StatefulWidget {
+  static String tag = 'home-page';
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  bool _loading = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DefaultAppBar().buildAppBar(),   // This is default app bar used in all modules
+      drawer: SideDrawer(),   // This is sideDrawer used in all modules
+      body: ListView(
+        shrinkWrap: true,
+        physics: ClampingScrollPhysics(),
+        children: [
+          Card(
+            elevation: 2.0,
+            margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+            shadowColor: Colors.black,
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  width: 170.0,
+                  height: 170.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/unknown.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Piyush Agrawal',
+                  style: TextStyle(fontSize: 20.0, color: Colors.black),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'CSE Student',
+                  style: TextStyle(fontSize: 15.0, color: Colors.black),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+              ],
+            ),
+          ),
+          Card(
+            color: Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    _loading = true;
+                    setState(() {
+                      _loading = true;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Notifications',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Icon(
+                          Icons.notifications_active_rounded,
+                          color:
+                              _loading ? Colors.deepOrangeAccent : Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _loading = false;
+                    setState(() {
+                      _loading = false;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'News',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Icon(
+                          Icons.email,
+                          color:
+                              _loading ? Colors.white : Colors.deepOrangeAccent,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          _loading
+              ? NotificationCard()
+              :  NewsCard(),
+        ],
+      ),
+    );
+  }
+
+
+}
+
---
+++ lib/screens/Login and Dashboard/login_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+bool checkBoxValue = false;
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 54.0,
+        child: Image.asset('assets/logo.jpg'),
+      ),
+    );
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Username/Email*',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password*',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: () {
+            Navigator.of(context).pushNamed('/home_page');
+          },
+          color: Colors.deepOrangeAccent,
+          child: Text(
+            'Login',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      ),
+    );
+    final forgotLabel = FlatButton(
+      onPressed: () {},
+      child: Text(
+        'Forgot password?',
+        style: TextStyle(color: Colors.black54),
+      ),
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            Text(
+              'Fusion Login',
+              style: TextStyle(
+                color: Colors.deepOrangeAccent,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 48.0),
+            email,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 8.0),
+            loginButton,
+            forgotLabel,
+          ],
+        ),
+      ),
+    );
+  }
+}
