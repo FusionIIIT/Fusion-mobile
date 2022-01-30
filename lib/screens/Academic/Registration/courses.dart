5ea557499067615e7a61ff18bee17fc4e2343f67
Arihant <jain.ari2000@gmail.com>
2022-01-30 23:45:54 +0530

    academic:registration updated

--- lib/screens/Academic/Registration/courses.dart
+++ lib/screens/Academic/Registration/courses.dart
@@ -8,41 +8,84 @@ class Courses extends StatefulWidget {
 }
 
 class _CoursesState extends State<Courses> {
-  getRows(List? map) {
-    List<DataRow> list = [];
-    map!.forEach((element) {
-      element.length > 0
-          ? list.add(DataRow(cells: [
-              DataCell(Text(element['course_id']['id'].toString())),
-              DataCell(Text(element['course_code'].toString())),
-              DataCell(Text(element['course_id']['course_name'].toString())),
-              DataCell(Text(element['credits'].toString())),
-              DataCell(Text(element['sem'].toString()))
-            ]))
-          : true;
-    });
-    return list;
+  getRows() {
+    return [
+      DataRow(cells: [
+        DataCell(Text('DS302')),
+        DataCell(Text('Engineering Design')),
+        DataCell(Text('4')),
+        DataCell(Text('5')),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('CS307')),
+        DataCell(Text('Computer Network')),
+        DataCell(Text('4')),
+        DataCell(Text('5')),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('CS308')),
+        DataCell(Text('Operating System')),
+        DataCell(Text('4')),
+        DataCell(Text('5')),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('CS309')),
+        DataCell(Text('Language Theory')),
+        DataCell(Text('4')),
+        DataCell(Text('5')),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('CS310a')),
+        DataCell(Text('Soft Computing')),
+        DataCell(Text('2')),
+        DataCell(Text('5')),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('CS310b')),
+        DataCell(Text('Parallel Computing')),
+        DataCell(Text('2')),
+        DataCell(Text('5')),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('CS311L')),
+        DataCell(Text('Lab based Project 2')),
+        DataCell(Text('2')),
+        DataCell(Text('5')),
+      ]),
+    ];
+
+    // map?.forEach((element) {
+    //   element.length > 0
+    //       ? list.add(DataRow(cells: [
+    //           DataCell(Text(element['course_id']['id'].toString())),
+    //           DataCell(Text(element['course_code'].toString())),
+    //           DataCell(Text(element['course_id']['course_name'].toString())),
+    //           DataCell(Text(element['credits'].toString())),
+    //           DataCell(Text(element['sem'].toString()))
+    //         ]))
+    //       : true;
+    // });
+    // list.add(DataRow(cells: ));
+    // return list;
   }
 
   @override
   Widget build(BuildContext context) {
-    List? finalData = widget.data.add_courses_options;
-    print(finalData);
+    // List? finalData = widget.data?.add_courses_options;
     return Container(
         child: Column(
       children: [
+        SizedBox(height: 20),
         Text("Available Courses for Next Semester"),
+        SizedBox(height: 10),
+        // if (finalData != null)
         SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: DataTable(columns: <DataColumn>[
               DataColumn(
-                label: Text("Course Id"),
-                numeric: false,
-              ),
-              DataColumn(
-                label: Text("Course Code"),
+                label: Text("Course ID"),
                 numeric: false,
               ),
               DataColumn(
@@ -57,9 +100,16 @@ class _CoursesState extends State<Courses> {
                 label: Text("Semester"),
                 numeric: false,
               )
-            ], rows: getRows(finalData)),
+            ], rows: getRows()),
           ),
         ),
+        // if (finalData == null)
+        // Expanded(
+        //     child: Center(
+        //         child: Text(
+        //   'Course List not available',
+        //   style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+        // )))
       ],
     ));
   }
