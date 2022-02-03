464c8f23aae92ec574f46865d546dbe675542770
Rishabh Negi <nrishabh20@gmail.com>
2022-02-03 17:29:20 +0530

    --fix: pdf view for current semester

--- lib/screens/Academic/Current_Semester/exam_time_table.dart
+++ lib/screens/Academic/Current_Semester/exam_time_table.dart
@@ -4,7 +4,6 @@ import 'package:fusion/Components/tabBar_text_button.dart';
 import 'package:fusion/constants.dart';
 import 'package:fusion/services/service_locator.dart';
 import 'package:fusion/services/storage_service.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ExamTimeTable extends StatefulWidget {
   @override
@@ -22,7 +21,7 @@ class _ExamTimeTableState extends State<ExamTimeTable> {
   }
 
   String getUrl({bool commonTTurl = false}) {
-    String url = 'https://' +
+    String url = 'http://' +
         getLink() +
         "/static/academic_procedures/END_SEM_EXAM_TT.pdf";
 
@@ -35,34 +34,21 @@ class _ExamTimeTableState extends State<ExamTimeTable> {
       length: 1,
       initialIndex: 0,
       child: Scaffold(
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
+        body: TabBarView(
           children: [
-            //
-            Flexible(
-              child: TabBarView(
-                children: [
-                  TabBarTextButton(
-                    label: 'END SEM TIME TABLE',
-                    onPressed: () async {
-                      String url = getUrl();
-                      if (await canLaunch(url)) {
-                        await launch(url, forceWebView: true);
-                      }
-
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute<dynamic>(
-                      //     builder: (_) => PDFViewerFromUrl(
-                      //       url: getUrl(),
-                      //       label: 'VIEW TIME-TABLE',
-                      //     ),
-                      //   ),
-                      // );
-                    },
+            TabBarTextButton(
+              label: 'END SEM TIME TABLE',
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<dynamic>(
+                    builder: (_) => PDFViewerFromUrl(
+                      url: getUrl(),
+                      label: 'END SEM TIME TABLE',
+                    ),
                   ),
-                ],
-              ),
+                );
+              },
             ),
           ],
         ),
