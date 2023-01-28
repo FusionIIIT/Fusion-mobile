551ae11312518f726b4a60d7679fbe25567f3d46
Avi2002Soy <86245437+Avi2002Soy@users.noreply.github.com>
2023-01-28 11:52:45 +0530

    Add files via upload

---
+++ lib/screens/estate_stock/stock_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'utils.dart';
+import 'page-1/stock-management-home.dart';
+// import 'package:myapp/page-1/stock-management-home.dart';
+// import 'package:myapp/page-1/stock-management-2.dart';
+// import 'package:myapp/page-1/rectangle-6.dart';
+// import 'package:myapp/page-1/fusion-ui-concept-1.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: MyCustomScrollBehavior(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Scene(),
+        ),
+      ),
+    );
+  }
+}
