78c58e9e297c1946738486268d97a3142babb7d0
Avi2002Soy <86245437+Avi2002Soy@users.noreply.github.com>
2023-03-18 15:17:41 +0530

    Update stock_page.dart

--- lib/screens/estate_stock/stock_page.dart
+++ lib/screens/estate_stock/stock_page.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
 import 'utils.dart';
-import 'page-1/stock-management-home.dart';
-import 'page-1/fusion-ui-login.dart';
-// import 'package:myapp/page-1/stock-management-home.dart';
-// import 'package:myapp/page-1/stock-management-2.dart';
-// import 'package:myapp/page-1/rectangle-6.dart';
-// import 'package:myapp/page-1/fusion-ui-concept-1.dart';
-
+import 'page-1/global dashboard.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      debugShowCheckedModeBanner: false,
-      scrollBehavior: MyCustomScrollBehavior(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+	@override
+	Widget build(BuildContext context) {
+	return MaterialApp(
+		title: 'Flutter',
+		debugShowCheckedModeBanner: false,
+		scrollBehavior: MyCustomScrollBehavior(),
+
+		theme: ThemeData(
+		primarySwatch: Colors.blue,
+		),
+		home: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Scene(),
       ),
-      home: Scaffold(
-        body: SingleChildScrollView(
-          child: Scene(),
-        ),
-      ),
-    );
-  }
+    ),
+	);
+	}
 }
+
