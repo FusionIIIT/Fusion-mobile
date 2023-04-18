c64955f02f3c10736b8320ef9eeba2a8b3961dd4
Avi2002Soy <86245437+Avi2002Soy@users.noreply.github.com>
2023-04-18 16:27:12 +0530

    StockInventoryModule appbar

--- lib/Components/appBar.dart
+++ lib/Components/appBar.dart
@@ -26,4 +26,32 @@ class DefaultAppBar {
       ],
     );
   }
+  
+  AppBar PS2Appbar() {
+    return AppBar(
+      iconTheme: IconThemeData(color: Colors.white),
+      title: Text(
+        "Stock Inventory Module",
+        style: TextStyle(
+            color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      backgroundColor: Color.fromARGB(255, 243, 108, 53),
+      // ignore: prefer_const_literals_to_create_immutables
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Icon(Icons.search),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Icon(Icons.notifications),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Icon(Icons.more_vert),
+        ),
+      ],
+      elevation: 0,
+    );
+  }
 }
