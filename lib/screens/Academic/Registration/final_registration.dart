7a465576957462231a543e03a81518ae899f68f7
Bhargavi Chetule <93151471+bchetule@users.noreply.github.com>
2023-04-22 22:39:04 +0530

    Update final_registration.dart

--- lib/screens/Academic/Registration/final_registration.dart
+++ lib/screens/Academic/Registration/final_registration.dart
@@ -1,74 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_html/shims/dart_ui_real.dart';
+import 'package:fusion/services/service_locator.dart';
+import 'package:fusion/services/storage_service.dart';
+import 'package:intl/intl.dart';
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32)),
+  ),
+);
 
 class FinalRegistration extends StatefulWidget {
-  final data;
-  const FinalRegistration({Key? key, this.data}) : super(key: key);
   @override
   _FinalRegistrationState createState() => _FinalRegistrationState();
 }
 
 class _FinalRegistrationState extends State<FinalRegistration> {
-  getRows(List? map) {
-    List<DataRow> list = [];
-    map?.forEach((element) {
-      element.length > 0
-          ? list.add(DataRow(cells: [
-              DataCell(Text(element[0]['course_id']['id'].toString())),
-              DataCell(Text(element[0]['course_code'].toString())),
-              DataCell(Text(element[0]['course_id']['course_name'].toString())),
-              DataCell(Text(element[0]['credits'].toString())),
-              DataCell(Text(element[0]['sem'].toString()))
-            ]))
-          : true;
-    });
-    return list;
-  }
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String? programme_type;
+  List programmeTypeItem = [
+    'B.Tech',
+    'B.des',
+    'M.Tech',
+    'PhD',
+  ];
+  String? scholarship_type;
+  List scholarshipTypeItem = [
+    'MCM',
+    'NSP',
+    'Random',
+  ];
+  String? academicyear;
+  List academicyearTypeItem = [
+    '1st',
+    '2nd',
+    '3rd',
+    '4th',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    List? finalData = widget.data?.final_registration_choices;
-    return Container(
-        child: Column(
-      children: [
-        SizedBox(height: 10),
-        Text("Final Choices Of Courses"),
-        if (finalData != null && finalData[0].length > 0)
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: DataTable(columns: <DataColumn>[
-                DataColumn(
-                  label: Text("Course ID"),
-                  numeric: false,
-                ),
-                DataColumn(
-                  label: Text("Course Code"),
-                  numeric: false,
-                ),
-                DataColumn(
-                  label: Text("Course Name"),
-                  numeric: false,
-                ),
-                DataColumn(
-                  label: Text("Credits"),
-                  numeric: false,
-                ),
-                DataColumn(
-                  label: Text("Semester"),
-                  numeric: false,
-                )
-              ], rows: getRows(finalData)),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: new Column(
+          children: [
+            new Container(
+              child: new Column(
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    "Final Reistration:",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        if (finalData == null || finalData[0].length == 0)
-          Expanded(
-              child: Center(
-                  child: Text(
-            'Final Registration Date is yet to come',
-            style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-          )))
-      ],
-    ));
+            new Container(
+                child: Column(children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(20),
+                child: Table(
+                  defaultColumnWidth: FixedColumnWidth(120.0),
+                  border: TableBorder.all(
+                      color: Colors.black, style: BorderStyle.solid, width: 2),
+                  children: [
+                    TableRow(children: [
+                      Column(children: [
+                        Text('Course ID', style: TextStyle(fontSize: 14.0))
+                      ]),
+                      Column(children: [
+                        Text('Course Code', style: TextStyle(fontSize: 14.0))
+                      ]),
+                      Column(children: [
+                        Text('User Name', style: TextStyle(fontSize: 14.0))
+                      ]),
+                      Column(children: [
+                        Text('Semester', style: TextStyle(fontSize: 14.0))
+                      ]),
+                    ]),
+                    // TableRow(children: [
+                    //   Column(children: [Text('Award_Name')]),
+                    //   Column(children: [Text('Student_Name')]),
+                    //   Column(children: [Text('2020')]),
+                    // ]),
+                  ],
+                ),
+              ),
+            ]))
+          ],
+        ),
+
+        //Table
+      ),
+    );
   }
 }
