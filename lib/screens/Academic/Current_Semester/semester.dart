fb59498b039ea500109f607229e2de3cebe6ef2d
Bhavik Agarwal <bhavikiitian@gmail.com>
2024-02-18 15:16:40 +0530

    added course list page

--- lib/screens/Academic/Current_Semester/semester.dart
+++ lib/screens/Academic/Current_Semester/semester.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fusion/models/academic.dart';
 import 'package:fusion/services/academic_service.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 class Semester extends StatefulWidget {
   // final data;
@@ -16,6 +17,12 @@ class _SemesterState extends State<Semester> {
   Widget build(BuildContext context) {
     // print(widget.data.currently_registered);
     print(widget.courseList);
+    
+    List<dynamic> courseList = jsonDecode(widget.courseList);
+
+    print(courseList);
+    print(courseList.runtimeType);
+
     return Container(
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
@@ -29,7 +36,7 @@ class _SemesterState extends State<Semester> {
                   numeric: false,
                 ),
                 DataColumn(
-                  label: Text("Course ID"),
+                  label: Text("Course Code"),
                   numeric: false,
                 ),
                 DataColumn(
@@ -45,17 +52,26 @@ class _SemesterState extends State<Semester> {
                   numeric: false,
                 )
               ],
-              rows: <DataRow>[
-                DataRow(
-                  cells: <DataCell>[
-                    DataCell(Text("")),
-                    DataCell(Text("OE2m07")),
-                    DataCell(Text("Operation Research")),
-                    DataCell(Text("3")),
-                    DataCell(Text("4")),
-                  ],
-                ),
-              ],
+              // rows: <DataRow>[
+              //   DataRow(
+              //     cells: <DataCell>[
+              //       DataCell(Text("")),
+              //       DataCell(Text("OE2m07")),
+              //       DataCell(Text("Operation Research")),
+              //       DataCell(Text("3")),
+              //       DataCell(Text("4")),
+              //     ],
+              //   ),
+              // ],
+              rows: courseList.map((data) {
+                return DataRow(cells: [
+                  DataCell(Text(data['course_id']['id'].toString())),
+                  DataCell(Text(data['course_code'].toString())),
+                  DataCell(Text(data['course_id']['course_name'].toString())),
+                  DataCell(Text(data['credits'].toString())),
+                  DataCell(Text(data['sem'].toString())),
+                ]);
+              }).toList(),
             ),
           ),
           // Text(
