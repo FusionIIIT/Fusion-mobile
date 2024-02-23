b29ba134be4e1e2e0a9741bbab0d9f44cf6133e9
yadavpritesh <yadavpritesh449@gmail.com>
2024-02-23 19:11:57 +0530

    21bcs247

--- lib/screens/Healthcenter/feedback.dart
+++ lib/screens/Healthcenter/feedback.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart'; // Import shared_preferences package
 
 class FeedBack extends StatefulWidget {
   @override
@@ -6,84 +9,115 @@ class FeedBack extends StatefulWidget {
 }
 
 class _FeedBackState extends State<FeedBack> {
-  @override
-  Widget feed = Container(
-    padding: EdgeInsets.all(20.0),
-    child: Column(
-      children: [
-        TextFormField(
-          maxLines: 8,
-          cursorHeight: 30,
-          decoration: new InputDecoration(
-            labelText: "Feedback",
-            fillColor: Colors.white,
-            border: new OutlineInputBorder(),
-            //fillColor: Colors.green
-          ),
-          style: new TextStyle(
-            fontFamily: "Poppins",
-          ),
-        ),
-        Center(
-          child: TextButton(
-            child: Text(
-              'Submit',
-              style: TextStyle(fontSize: 15.0),
-            ),
-            // color: Colors.deepOrangeAccent,
-            onPressed: () {},
-          ),
-        ),
-      ],
-    ),
-  );
+  final TextEditingController feedbackController = TextEditingController();
 
+  Future<String> getToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token') ?? ''; // Return empty string if token not found
+  }
+
+  Future<void> submitFeedback() async {
+    final String feedback = feedbackController.text;
+    final url = 'http://127.0.0.1:8000/healthcenter/api/student/request'; // Replace with your backend URL
+
+    try {
+      final token = await getToken(); // Fetch token from local storage
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Token $token', // Use fetched token
+        },
+        body: jsonEncode({'complaintadd': true, 'feedback': feedback}),
+      );
+
+      if (response.statusCode == 201) {
+        print('Feedback submitted successfully');
+      } else {
+        print('Failed to submit feedback: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Exception occurred: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text("Health Center"),
+        title: Text("Health Center Feedback"),
         backgroundColor: Colors.black,
       ),
       body: Container(
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              DefaultTabController(
-                  length: 1, // length of tabs
-                  initialIndex: 0,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Container(
-                          //color: Colors.deepOrangeAccent,
-                          child: TabBar(
-                            labelColor: Colors.deepOrange,
-                            indicatorColor: Colors.deepOrangeAccent,
-                            unselectedLabelColor: Colors.black,
-                            tabs: [
-                              Tab(
-                                child: Text(
-                                  "Feedback Form",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ],
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            DefaultTabController(
+              length: 1, // length of tabs
+              initialIndex: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    child: TabBar(
+                      labelColor: Colors.deepOrange,
+                      indicatorColor: Colors.deepOrangeAccent,
+                      unselectedLabelColor: Colors.black,
+                      tabs: [
+                        Tab(
+                          child: Text(
+                            "Feedback Form",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
                         ),
-                        Container(
-                            height: 520, //height of TabBarView
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    top: BorderSide(
-                                        color: Colors.grey, width: 0.5))),
-                            child: TabBarView(children: <Widget>[
-                              feed,
-                            ]))
-                      ])),
-            ]),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 520, //height of TabBarView
+                    decoration: BoxDecoration(
+                      border: Border(
+                        top: BorderSide(color: Colors.grey, width: 0.5),
+                      ),
+                    ),
+                    child: TabBarView(
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            SizedBox(height: 20.0),
+                            TextFormField(
+                              controller: feedbackController,
+                              maxLines: 8,
+                              cursorHeight: 30,
+                              decoration: InputDecoration(
+                                labelText: "Feedback",
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(),
+                              ),
+                              style: TextStyle(
+                                fontFamily: "Poppins",
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            ElevatedButton(
+                              onPressed: submitFeedback,
+                              child: Text('Submit'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
