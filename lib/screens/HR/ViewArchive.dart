316c1bb8db9cea679673f79254528135c97b6430
praneki <pranjaljha00@gmail.com>
2024-04-24 18:54:50 +0530

    import 'dart:ffi' commented

--- lib/screens/HR/ViewArchive.dart
+++ lib/screens/HR/ViewArchive.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:fusion/screens/HR/ForwardCPDAAdvance.dart';
--- lib/screens/HR/ViewInbox.dart
+++ lib/screens/HR/ViewInbox.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:fusion/Components/appBar2.dart';
