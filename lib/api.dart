d09a5bc3f9bc52d0290f29b3c212d61f9c420215
Arpan Karjee <arpankarjee2004@gmail.com>
2024-05-05 18:53:35 +0530

    final changes

--- lib/api.dart
+++ lib/api.dart
@@ -1,9 +1,9 @@
 //Server and local links
-String klocalLink = "127.0.0.1:8000";
-String kserverLink = "192.168.137.1:8000";
+String klocalLink = "172.27.16.214:8000";
+String kserverLink = "172.27.16.214:8000";
 
 //Login Service
-String kAuthUrl = "192.168.137.1:8000";
+String kAuthUrl = "172.27.16.214:8000";
 String kAuthLogin = "/api/auth/login/";
 // String kAuthLogin = "/accounts/login";
 
