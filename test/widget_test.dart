d5aaea470994af556b73bbc6a77407e404f54b23
aish0749 <aishwaryasaxena57@gmail.com>
2024-05-06 00:41:44 +0530

    fix unneeded merges

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,10 +6,10 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutterz_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fusion/main.dart';
-
+  
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
