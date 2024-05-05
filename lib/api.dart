6c6db2e9aa0a315efd87f1f44a9459b006b485a2
Jatin Yadav <jatinyadav@Jatins-MacBook-Pro.local>
2024-05-05 09:23:17 +0530

    Resolved Conflicts

--- lib/api.dart
+++ lib/api.dart
@@ -2,9 +2,11 @@
 String klocalLink = "127.0.0.1:8000";
 String kserverLink = "172.27.16.214:8000";
 
+
 //Login Service
 String kAuthUrl = "172.27.16.214:8000";
 String kAuthLogin = "/api/auth/login/";
+
 
 
 
